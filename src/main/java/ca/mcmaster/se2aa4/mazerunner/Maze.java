5fdedca534b0a3a1c4071ce5b20e5a36f65e9619
Akshika Thiyagendran <takshika2005@gmail.com>
2025-01-24 17:16:40 -0500

    refactor: created a 2d array of the maze

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -4,6 +4,7 @@ package ca.mcmaster.se2aa4.mazerunner;
  * Description: Reads the tect file for which board to use and prints it. Prints a message if board not found.
  */
 
+ import java.util.ArrayList;
  import java.io.BufferedReader;
  import java.io.File;
  import java.io.FileReader;
@@ -13,6 +14,8 @@ package ca.mcmaster.se2aa4.mazerunner;
 public class Maze {
     private static final Logger logger = LogManager.getLogger();
     public String filepath;
+    private char [][] maze;
+    private int rows, cols;
 
     public Maze (String filepath) {                             // identifying the maze being used
         this.filepath = filepath;
@@ -25,16 +28,18 @@ public class Maze {
             logger.info("**** Reading the maze from file " + filepath);
             BufferedReader reader = new BufferedReader(new FileReader(filepath));
             String line;
+
+            ArrayList <String> mazeTextFileLines = new ArrayList<>();                           // contains the row data from the text file        
             while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        logger.trace("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        logger.trace("PASS ");
-                    }
-                }
-                System.out.print(System.lineSeparator());
+                mazeTextFileLines.add(line);
             }
+
+            // initializing the maze w/ a 2d array
+            rows = mazeTextFileLines.size();
+            cols = mazeTextFileLines.get(0).length();
+            maze = new char[rows][cols];
+            logger.info("Maze read into 2d array successfully.");
+
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
